55da18defc59584202e390b366c169b1275ca05f
core_bit <nkorbit@outlook.com>
2024-05-07 18:10:54 +0200

    Experimental: FastEGN with gradient of the true class only

--- somax/__init__.py
+++ somax/__init__.py
@@ -9,6 +9,7 @@ from somax.hf.newton_cg import NewtonCG
 
 # Gauss-Newton Methods
 from somax.gn.egn import EGN
+from somax.gn.fast_egn import FastEGN
 from somax.gn.gnb import GNB
 from somax.gn.ignd import IGND
 from somax.gn.lb import LB
