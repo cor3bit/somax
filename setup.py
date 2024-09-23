7c107c5a94ce4447c08bc596e9c89ed5c95bb82b
core_bit <nkorbit@outlook.com>
2024-09-23 15:20:16 +0200

    Add explicit encoding when reading readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
 
 
 setuptools.setup(
